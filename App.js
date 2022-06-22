e3cd1ab475f0d991dd28aaf2642fa6606432e5eb
wissal <81393549+wissal91@users.noreply.github.com>
2022-06-22 19:48:29 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import { NavigationContainer } from '@react-navigation/native';
+import Screens from './stack/index';
+
+export default function App() {
+  return (
+    <NavigationContainer><Screens/></NavigationContainer>
+  );
+}
